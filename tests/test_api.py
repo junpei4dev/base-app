c50d011710047fe9e85e03de09e491fc068aa53c
junpei4dev <junpei4dev@gmail.com>
2023-11-10 01:53:20 +0900

    add doc

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,6 +6,12 @@ client = TestClient(app)
 
 
 def test_health_check() -> None:
+    """
+    Test the health check endpoint of the API.
+
+    Sends a GET request to the '/health-check' endpoint and asserts that the response
+    status code is 200 and that the response data contains a 'status' key with the value 'ok'.
+    """
     response = client.get(
         "/health-check",
     )
